b209f871ebaf7715db6b5593182def0e74ad5b3b
Barito06 <aguerocalderonhectorjoaquin@gmail.com>
2024-04-09 17:43:20 -0300

    Initial commit

---
+++ src/practico5/Practico5.java
@@ -0,0 +1,12 @@
+package practico5;
+
+public class Practico5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
